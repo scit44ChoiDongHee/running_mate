081f6f53cf713eb3ca82096efc2200ea83106f4a
scit44ChoiDongHee <scit44ChoiDongHee@gmail.com>
2023-09-21 14:57:20 +0900

    sql 파일 추가

--- running_mate.sql
+++ running_mate.sql
@@ -1,28 +1,19 @@
 create table user_vo (
-    user_id         varchar2(20)    primary key,
-    user_pw         varchar2(20)    not null,
-    user_name       varchar2(20)    not null,
-    user_email      varchar2(30)    not null,
-    user_birthday   varchar2(20),
-    user_phone      varchar2(20)    not null,
-    user_nickname   varchar2(20)    not null,
+    user_id         varchar2(100)    primary key,
+    user_pw         varchar2(100)    not null,
+    user_name       varchar2(100)    not null,
+    user_email      varchar2(100)    not null,
+    user_birthday   varchar2(100)    not null,
+    user_phone      varchar2(100)    not null,
+    user_nickname   varchar2(100)    not null,
     user_photo      varchar2(200)
 );
 
 insert into 
-    user_vo (user_id, user_pw, user_name, user_email, user_phone, user_nickname)
+    user_vo (user_id, user_pw, user_name, user_email, user_birthday, user_phone, user_nickname, user_photo)
 values 
-    ('admin', 'admin', 'ȫ�浿', 'admin@gmail.com', '010-1111-1111', 'admin');
+    ('admin', 'admin', 'ȫ�浿', 'admin@gmail.com', '2000-01-01', '010-1111-1111', 'admin', 'profile_image');
     
 select * from user_vo;
-
 drop table user_vo;
-
-create table user_vo (
-    user_id         varchar2(100)    primary key,
-    user_pw         varchar2(100)    not null
-);
-
 commit;
-
-delete from user_vo where user_id = "CHA";
