f68f18cbd0e982710c78357e922a749477f2ca81
shin sang ho <dntkrk@gmail.com>
2023-10-02 21:43:46 +0900

    일과 모달 구현

--- src/main/frontend/src/views/demo/daily.jsx
+++ src/main/frontend/src/views/demo/daily.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import CalendarView from '../../components/shared/CalendarView'
+import FullCalendarView from '../../components/ui/FullCalenderView/FullCalenderView'
+import InsertDailyGoalPlanModal from '../../components/ui/InsertDailyGoalPlanModal/InsertDailyGoalPlanModal'
+
+
 export default function daily() {
   return (
     
     <div>
-      <CalendarView/>
+      <InsertDailyGoalPlanModal/>
     </div>
 
   )
--- src/main/frontend/vite.config.ts
+++ src/main/frontend/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
                 target: 'http://localhost:8888',
                 changeOrigin: true,
                 secure: false,
-                rewrite: (path) => path.replace(/^\/api/, ''),
+                // rewrite: (path) => path.replace(/^\/api/, ''),
             },
         },
     }, // 프록시 설정 끝
